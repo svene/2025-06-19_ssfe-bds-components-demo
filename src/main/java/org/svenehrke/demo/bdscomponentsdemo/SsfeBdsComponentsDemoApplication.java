75054c462a293c4758b88c1f58d8aa7439d6095e
Sven Ehrke <sven.ehrke@baloise.ch>
2025-06-19 16:01:16 +0200

    initial springboot files

---
+++ src/main/java/org/svenehrke/demo/bdscomponentsdemo/SsfeBdsComponentsDemoApplication.java
@@ -0,0 +1,13 @@
+package org.svenehrke.demo.bdscomponentsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfeBdsComponentsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsfeBdsComponentsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/svenehrke/demo/bdscomponentsdemo/SsfeBdsComponentsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.svenehrke.demo.bdscomponentsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfeBdsComponentsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
