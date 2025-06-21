660423a43eb8994c692445bf05164aae82944a8d
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-06-21 19:27:33 +0200

    working on 'heading' component

---
+++ src/main/java/jte/HeadingProps.java
@@ -0,0 +1,16 @@
+package jte;
+
+public record HeadingProps(Integer level) {
+
+	public String classes() {
+		String size = switch (level) {
+			case 2 -> "xx-large";
+			case 3 -> "x-large";
+			case 4 -> "large";
+			case 5 -> "normal";
+			default -> "xxx-large";
+		};
+		return "bal-heading bal-heading--level-h%s bal-heading__text bal-heading__text--color-success bal-heading__text--size-%s"
+			.formatted(level, size);
+	}
+}
