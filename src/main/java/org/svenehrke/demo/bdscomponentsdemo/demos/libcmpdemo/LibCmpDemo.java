8f9564430800cdf7a5afed4aef59d14c76c52cef
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-07-06 08:52:24 +0200

    refactoring: finished

---
+++ src/main/java/org/svenehrke/demo/bdscomponentsdemo/demos/libcmpdemo/LibCmpDemo.java
@@ -0,0 +1,29 @@
+package org.svenehrke.demo.bdscomponentsdemo.demos.libcmpdemo;
+
+import de.tschuehly.spring.viewcomponent.core.component.ViewComponent;
+import de.tschuehly.spring.viewcomponent.jte.ViewContext;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.svenehrke.demo.bdscomponentsdemo.shared.RequestViewContext;
+
+@ViewComponent
+@RequiredArgsConstructor
+@Controller
+@Slf4j
+public class LibCmpDemo {
+    private final RequestViewContext rvc;
+
+    public static final String URL = "/demo/libcmp";
+    public static final String TITLE = "Library-Component Demo";
+
+    public record Ctx(
+        RequestViewContext rvc
+    ) implements ViewContext {}
+
+    @GetMapping(URL)
+    public ViewContext ctx() {
+        return new Ctx(rvc);
+    }
+}
